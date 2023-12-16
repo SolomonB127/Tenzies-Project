6d3821b15628e14805de7a19d6c9cc34c72674c7
SolomonB127 <solomonakinsulire@gmail.com>
2023-12-16 17:06:28 +0000

    Added a Rolls Count function and a falling dice video background

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Die from './assets/Components/Die';
 import './App.css'
 import {nanoid} from "nanoid"
 import Confetti from 'react-confetti'
-
+import Video from './assets/hd1722.mov'
 
 function App() {
 
@@ -30,8 +30,7 @@ function App() {
 
   const [tenzies, setTenzies] = useState(false);
 
-
-
+  const [count, setCount] = useState(0);
   useEffect(()=>{
    const allIsHeld = dieState.every(die => die.isHeld);
    const firstValue = dieState[0].value
@@ -48,6 +47,7 @@ function App() {
   };
 
   function rollDice(){
+    setCount(prevCount => prevCount + 1); 
     if(!tenzies){
       setDieState(oldRoll => oldRoll.map(roll => {
         return roll.isHeld ? roll : generateNewDie()
@@ -55,8 +55,8 @@ function App() {
     } else{
       setTenzies(false);
       setDieState(allNewDice())
+      setCount(0)
     }
-    
   };
 
   const dieData = dieState.map(die => {
@@ -70,6 +70,9 @@ function App() {
 
   return (
     <>
+     <div style={{ position: "fixed", right: 0, bottom: 0, minWidth: "100%", minHeight: "100%", zIndex: -1 }}>
+      <video src={Video} autoPlay loop muted type="video/mp4"></video>
+    </div>
     {tenzies && <Confetti />}
       <div className='App'>
           <h1 className="title">Tenzies</h1>
@@ -82,6 +85,9 @@ function App() {
           <div className='die--container'>
           {dieData}
           </div>
+
+          <b>No of times Rolled: {count}</b>
+
           <button  
            className= "roll--dice"
             onClick={rollDice}
